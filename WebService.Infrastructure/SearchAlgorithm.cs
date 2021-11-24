afc179afd4e2f20fbf70a3987a86efd569d29246
Myssenberg <my.nordaljensen@gmail.com>
2021-11-24 14:22:57 +0100

    SearchAlgorithm and SearchAlgorithmTests added

---
+++ WebService.Infrastructure/SearchAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿namespace WebService.Core.Server
+{
+    public class SearchAlgorithm
+    {
+        public List<int> Search(List<string> inputWords, List<int> tagIDs, List<List<int>> filterIDs)
+        {
+            throw new NotImplementedException();
+
+            //return new List<int>();
+        }
+    }
+}
