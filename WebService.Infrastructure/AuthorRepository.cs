5a33c7d63ffaf54b7bf3f66a2f98d42b13cdaa65
RasmusUK <rasmus.kritensen@live.dk>
2021-11-24 22:02:35 +0100

    Added author repository (also as interface) since we had to create author as an entity to allow it in the dbcontext

---
+++ WebService.Infrastructure/AuthorRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace WebService.Infrastructure
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        private readonly IContext _context;
+
+        public AuthorRepository(IContext context)
+        {
+            _context = context;
+        }
+        public Task<(Status, AuthorDTO)> CreateAsync(CreateAuthorDTO language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Status> DeleteAsync(int languageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(Status, AuthorDTO)> ReadAsync(int languageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyCollection<AuthorDTO>> ReadAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Status> UpdateAsync(AuthorDTO languageDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
