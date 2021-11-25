d3f8fe56440e2c44166f3d48b7914bd970c8e220
RasmusUK <rasmus.kritensen@live.dk>
2021-11-25 18:28:58 +0100

    Tag repository and tests implemented

---
+++ WebService.Infrastructure.Tests/ContextRepositoryTests/TagRepositoryTests.cs
@@ -0,0 +1,179 @@
+﻿namespace WebService.Infrastructure.Tests.ContextRepositoryTests
+{
+    public class TagRepositoryTests
+    {
+        private readonly TestVariables _v;
+
+        public TagRepositoryTests()
+        {
+            _v = new TestVariables();
+        }
+        #region Create
+        [Fact]
+        public async Task CreateAsync_tag_returns_new_tag_with_id()
+        {
+            var tag = new CreateTagDTO("Database", 30);
+
+            var actual = await _v.TagRepository.CreateAsync(tag);
+
+            var expected = (Status.Created, new TagDTO(4, "Database", 30));
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task CreateAsync_tag_returns_conflict_and_existing_tag()
+        {
+            var tag = new CreateTagDTO("API", 90);
+
+            var actual = await _v.TagRepository.CreateAsync(tag);
+
+            var expected = (Status.Conflict, new TagDTO(3, "API", 90));
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task CreateAsync_tag_returns_count_one_more()
+        {
+            var tag = new CreateTagDTO("Database", 30);
+
+            await _v.TagRepository.CreateAsync(tag);
+
+            var actual = _v.TagRepository.ReadAsync().Result.Count;
+
+            var expected = 4;
+
+            Assert.Equal(expected, actual);
+        }
+        #endregion
+
+        #region Read
+
+        [Fact]
+        public async Task ReadAsync_tag_by_id_returns_tag_and_status_found()
+        {
+            var actual = await _v.TagRepository.ReadAsync(1);
+
+            var expected = (Status.Found, new TagDTO(1, "SOLID", 10));
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task ReadAsync_tag_by_id_returns_empty_tag_and_status_notFound()
+        {
+            var actual = await _v.TagRepository.ReadAsync(4);
+
+            var expected = (Status.NotFound, new TagDTO(-1, "", 0));
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task ReadAllAsync_returns_all_tags()
+        {
+            var actuals = await _v.TagRepository.ReadAsync();
+            
+            var expected1 = new TagDTO(1, "SOLID", 10);
+            var expected2 = new TagDTO(2, "RAD", 50);
+            var expected3 = new TagDTO(3, "API", 90);
+
+            Assert.Collection(actuals,
+                actual => Assert.Equal(expected1, actual),
+                actual => Assert.Equal(expected2, actual),
+                actual => Assert.Equal(expected3, actual));
+        }
+
+        #endregion
+
+        #region Delete
+
+        [Fact]
+        public async Task DeleteAsync_tag_by_id_returns_status_deleted()
+        {
+            var actual = await _v.TagRepository.DeleteAsync(1);
+
+            var expected = Status.Deleted;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_tag_by_id_returns_status_notFound()
+        {
+            var actual = await _v.TagRepository.DeleteAsync(4);
+
+            var expected = Status.NotFound;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_tag_by_id_returns_count_one_less()
+        {
+            await _v.TagRepository.DeleteAsync(3);
+
+            var actual = _v.TagRepository.ReadAsync().Result.Count;
+
+            var expected = 2;
+
+            Assert.Equal(expected, actual);
+        }
+
+        #endregion
+
+        #region Update
+        [Fact]
+        public async Task UpdateAsync_tag_by_id_returns_status_updated()
+        {
+            var updateTagDTO = new TagDTO(3, "Database", 30);
+
+            var actual = await _v.TagRepository.UpdateAsync(updateTagDTO);
+
+            var expected = Status.Updated;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_tag_by_id_read_updated_returns_status_found_and_updated_tag()
+        {
+            var updateTagDTO = new TagDTO(3, "Database", 30);
+
+            await _v.TagRepository.UpdateAsync(updateTagDTO);
+
+            var actual = await _v.TagRepository.ReadAsync(3);
+
+            var expected = (Status.Found, updateTagDTO);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_tag_by_id_returns_status_notFound()
+        {
+            var updateTagDTO = new TagDTO(4, "Database", 30);
+
+            var actual = await _v.TagRepository.UpdateAsync(updateTagDTO);
+
+            var expected = Status.NotFound;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_tag_by_id_returns_status_conflict()
+        {
+            var updateTagDTO = new TagDTO(3, "SOLID", 10);
+
+            var actual = await _v.TagRepository.UpdateAsync(updateTagDTO);
+
+            var expected = Status.Conflict;
+
+            Assert.Equal(expected, actual);
+        }
+
+        #endregion
+    }
+}
--- WebService.Infrastructure/TagRepository.cs
+++ WebService.Infrastructure/TagRepository.cs
@@ -8,29 +8,78 @@
             _context = context;
         }
 
-        public Task<(Status, TagDTO)> CreateAsync(CreateTagDTO tag)
+        public async Task<(Status, TagDTO)> CreateAsync(CreateTagDTO tag)
         {
-            throw new NotImplementedException();
+            var existing = await(from t in _context.Tags
+                                 where t.Name == tag.Name
+                                 where t.Weight == tag.Weight
+                                 select new TagDTO(t.Id, t.Name, t.Weight))
+                           .FirstOrDefaultAsync();
+
+            if (existing != null) return (Status.Conflict, existing);
+
+            var entity = new Tag(tag.Name, tag.Weight);
+
+            _context.Tags.Add(entity);
+
+            await _context.SaveChangesAsync();
+
+            return (Status.Created, new TagDTO(entity.Id, entity.Name, entity.Weight));
         }
 
-        public Task<Status> DeleteAsync(int tagId)
+        public async Task<Status> DeleteAsync(int tagId)
         {
-            throw new NotImplementedException();
+            var tag = await _context.Tags.FindAsync(tagId);
+
+            if (tag == null) return Status.NotFound;
+
+            _context.Tags.Remove(tag);
+
+            await _context.SaveChangesAsync();
+
+            return Status.Deleted;
         }
 
-        public Task<(Status, TagDTO)> ReadAsync(int tagId)
+        public async Task<(Status, TagDTO)> ReadAsync(int tagId)
         {
-            throw new NotImplementedException();
+            var query = from t in _context.Tags
+                        where t.Id == tagId
+                        select new TagDTO(t.Id, t.Name, t.Weight);
+
+            var category = await query.FirstOrDefaultAsync();
+
+            if (category == null) return (Status.NotFound, new TagDTO(-1, "", 0));
+
+            return (Status.Found, category);
         }
 
-        public Task<IReadOnlyCollection<TagDTO>> ReadAsync()
+        public async Task<IReadOnlyCollection<TagDTO>> ReadAsync()
         {
-            throw new NotImplementedException();
+            return await _context.Tags.Select(t => new TagDTO(t.Id, t.Name, t.Weight)).ToListAsync();
+
         }
 
-        public Task<Status> UpdateAsync(TagDTO tagDTO)
+        public async Task<Status> UpdateAsync(TagDTO tagDTO)
         {
-            throw new NotImplementedException();
+            var existing = await(from t in _context.Tags
+                                 where t.Id != tagDTO.Id
+                                 where t.Name == tagDTO.Name
+                                 where t.Weight == tagDTO.Weight
+                                 select new TagDTO(t.Id, t.Name, t.Weight))
+                          .AnyAsync();
+
+            if (existing) return Status.Conflict;
+
+            var entity = await _context.Tags.FindAsync(tagDTO.Id);
+
+            if (entity == null) return Status.NotFound;
+
+            entity.Name = tagDTO.Name;
+            entity.Weight = tagDTO.Weight;
+
+            _context.SaveChanges();
+
+            return Status.Updated;
         }
     }
 }
