8548eb2d092c6bac15c9d73898e44316447448bf
Thor Lyster Lind <thll@itu.dk>
2021-12-07 22:35:59 +0100

    Finished GUI, SearchController.post throws not implemented

--- WebService.Core/Server/Controllers/SearchController.cs
+++ WebService.Core/Server/Controllers/SearchController.cs
@@ -19,49 +19,7 @@
         public async Task<ActionResult<ICollection<MaterialDTO>>> Post(SearchForm searchForm)
         {
             //var result = await _search.Search(searchForm);
-            //throw new NotImplementedException();
-            
-            if (searchForm.TextField == "search")
-            {
-                return new List<MaterialDTO>
-                {
-                    new(
-                        0,
-                        new List<CreateWeightedTagDTO>(),
-                        new List<CreateRatingDTO>(),
-                        new List<CreateLevelDTO>(),
-                        new List<CreateProgrammingLanguageDTO>(),
-                        new List<CreateMediaDTO>(),
-                        new LanguageDTO(0,"English"),
-                        "search",
-                        "search.com",
-                        "search",
-                        "search",
-                        new List<CreateAuthorDTO>(),
-                        DateTime.Now
-                    )
-                };
-            }
-
-            return new List<MaterialDTO>
-            {
-                new(
-                    1,
-                    new List<CreateWeightedTagDTO>(),
-                    new List<CreateRatingDTO>(),
-                    new List<CreateLevelDTO>(),
-                    new List<CreateProgrammingLanguageDTO>(),
-                    new List<CreateMediaDTO>(),
-                    new LanguageDTO(0,"English"),
-                    "no-search",
-                    "no-search.com",
-                    "no-search",
-                    "no-search",
-                    new List<CreateAuthorDTO>(),
-                    DateTime.Now
-                )
-            };
-
+            throw new NotImplementedException();
         }
     }
 }
