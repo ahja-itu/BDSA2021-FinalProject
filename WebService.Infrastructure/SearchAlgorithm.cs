dacbb5473420a64238f2dfd420e4005edd264eb7
RasmusUK <rasmus.kritensen@live.dk>
2021-12-07 10:39:56 +0100

    Removed ISearch that should only be in shared. Added the interface to the SearchAlgorithm to add it in the scope in the server.

--- WebService.Infrastructure/SearchAlgorithm.cs
+++ WebService.Infrastructure/SearchAlgorithm.cs
@@ -1,12 +1,16 @@
 ﻿namespace WebService.Core.Server
 {
-    public class SearchAlgorithm
+    public class SearchAlgorithm : ISearch
     {
         public List<int> Search(List<string> inputWords, List<int> tagIDs, List<List<int>> filterIDs)
         {
             throw new NotImplementedException();
 
             //return new List<int>();
+        }
+        public Task<ICollection<MaterialDTO>> Search(SearchForm searchForm)
+        {
+            throw new NotImplementedException();
         }
     }
 }
