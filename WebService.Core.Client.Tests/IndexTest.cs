eb377c32a637ab157a8a5120b2837c53e70c654d
Thor Lyster Lind <thll@itu.dk>
2021-12-08 15:09:35 +0100

    test of mock http client

--- WebService.Core.Client.Tests/IndexTest.cs
+++ WebService.Core.Client.Tests/IndexTest.cs
@@ -1,48 +1,122 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using Xunit;
 using Bunit;
-using WebService.Core.Client.Pages;
 using AngleSharp.Dom;
+using WebService.Core.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using RichardSzalay.MockHttp;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using Index = WebService.Core.Client.Pages.Index;
 
-/*
 namespace WebService.Core.Client.Tests;
 
-public class IndexTest
+#region MockHttpClient
+
+public static class MockHttpClientBunitHelpers
 {
-    
-    [Fact]
-    public void IndexContainsWebsiteName()
+    public static MockHttpMessageHandler AddMockHttpClient(this TestServiceProvider services)
     {
-        // Arrange
-        using var ctx = new TestContext();
+        var mockHttpHandler = new MockHttpMessageHandler();
+        var httpClient = mockHttpHandler.ToHttpClient();
+        httpClient.BaseAddress = new System.Uri("http://localhost:44355/");
+        services.AddSingleton<HttpClient>(httpClient);
+        return mockHttpHandler;
+    }
 
-        var cut = ctx.RenderComponent<Index>();
+    public static MockedRequest RespondJson<T>(this MockedRequest request, T content)
+    {
+        request.Respond(req =>
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonSerializer.Serialize(content));
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return response;
+        });
+        return request;
+    }
 
-        // Act
-        var name = cut.Find("h1");
+    public static MockedRequest RespondJson<T>(this MockedRequest request, System.Func<T> contentProvider)
+    {
+        request.Respond(req =>
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonSerializer.Serialize(contentProvider()));
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return response;
+        });
+        return request;
+    }
+}
 
-        // Assert
-        Assert.Equal("Bridge The Gap!", name.GetInnerText());
+#endregion
+
+public class IndexTest : IDisposable
+{
+    private TestContext _ctx;
+    private MockHttpMessageHandler _mock;
+    public IndexTest() 
+    {
+        var languages = new List<LanguageDTO>
+        {
+            new (0,"english"),
+            new (1,"danish")
+        };
+        var levels = new List<LevelDTO>
+        {
+            new (0,"High School"),
+            new (1, "Middle School"),
+            new (2, "P.H.D")
+        };
+        var medias = new List<MediaDTO>
+        {
+            new (0,"Book"),
+            new (1,"video"),
+            new(2,"quiz")
+        };
+        var programmingLanguages = new List<ProgrammingLanguageDTO>
+        {
+            new (0,"c#"),
+            new (1,"java"),
+            new (2, "c++"),
+            new (3,"f#")
+        };
+        var tags = new List<TagDTO>
+        {
+            new (0,"c#"),
+            new (1,"GOF"),
+            new (2, "Docker")
+        };
+        
+        _ctx = new TestContext();
+        _mock = _ctx.Services.AddMockHttpClient();
+        _mock.When("/Tag").RespondJson(tags);
+        _mock.When("/Level").RespondJson(levels);
+        _mock.When("/ProgrammingLanguage").RespondJson(programmingLanguages);
+        _mock.When("/Language").RespondJson(languages);
+        _mock.When("/Media").RespondJson(medias);
+        
     }
 
     [Fact]
     public void IndexFilterButtonShowsFilterOptions()
     {
         // Arrange
-        using var ctx = new TestContext();
-        var cut = ctx.RenderComponent<Index>();
-        var buttons = cut.FindAll("button");
+        var cut = _ctx.RenderComponent<Index>();
+        var buttons = cut.FindAll("button"); 
         var filterButton = buttons.GetElementById("filterButton");
     
         // Act
         filterButton.Click();
         var filterOptions = new List<IElement>();
-        var buttonsAfterClick = cut.FindAll("button");
+        var buttonsAfterClick = cut.FindAll("button"); 
         for (var i = 0; i < 1000; i++)
         {
             var filterOption = buttonsAfterClick.GetElementById("FilterOption " + i);
-            if (filterOption != null) filterOptions.Add(filterOption);
+            if (filterOption != null) filterOptions.Add(filterOption); 
         }
         var countOfFilterOptions = cut.FindAll("button").Count - buttons.Count - filterOptions.Count;
         
@@ -54,8 +128,7 @@ public class IndexTest
     public void IndexFilterButtonAgainHidesFilterOptions()
     {
         // Arrange
-        using var ctx = new TestContext();
-        var cut = ctx.RenderComponent<Index>();
+        var cut = _ctx.RenderComponent<Index>();
         var buttonsBefore = cut.FindAll("button");
         var filterButton = buttonsBefore.GetElementById("filterButton");
 
@@ -72,29 +145,28 @@ public class IndexTest
     public void IndexChangeSearchFieldForFilterWhenSelectingOptionTags()
     {
         // Arrange
-        using var ctx = new TestContext();
-        var cut = ctx.RenderComponent<Index>();
-        var filterButton = cut.FindAll("button").GetElementById("filterButton");
+        var cut = _ctx.RenderComponent<Index>();
+        var expected = 12;
 
         // Act
+        var filterButton = cut.FindAll("button").GetElementById("filterButton");
         filterButton.Click();
         cut.FindAll("button").GetElementById("tags").Click();
-        var searchField = cut.FindAll("input").GetElementById("searchFilter");
+        var actual = cut.FindAll("button").Count;
 
         // Assert
-        Assert.Equal("Search Tags",searchField.GetAttribute("Placeholder"));
+        Assert.Equal(expected, actual);
     }
 
     [Fact]
     public void IndexHideSearchFieldForFilterWhenSelectingOptionRatingsAndShowRangeSlider()
     {
         // Arrange
-        using var ctx = new TestContext();
-        ctx.JSInterop.SetupVoid("Radzen.createSlider", _ => true);
-        var cut = ctx.RenderComponent<Index>();
+        _ctx.JSInterop.SetupVoid("Radzen.createSlider", _ => true);
+        var cut = _ctx.RenderComponent<Index>();
         var filterButton = cut.FindAll("button").GetElementById("filterButton");
-        bool searchFieldIsGone = true;
-        bool sliderIsGone = false;
+        var searchFieldIsGone = true;
+        var sliderIsGone = false;
 
         // Act
         filterButton.Click();
@@ -111,68 +183,74 @@ public class IndexTest
     public void IndexChangeSearchFieldForFilterWhenSelectingOptionLevels()
     {
         // Arrange
-        using var ctx = new TestContext();
-        var cut = ctx.RenderComponent<Index>();
-        var filterButton = cut.FindAll("button").GetElementById("filterButton");
+        
+        var cut = _ctx.RenderComponent<Index>();
+        var expected = 12;
 
         // Act
+        var filterButton = cut.FindAll("button").GetElementById("filterButton");
         filterButton.Click();
         cut.FindAll("button").GetElementById("levels").Click();
-        var searchField = cut.FindAll("input").GetElementById("searchFilter");
-
+        var actual = cut.FindAll("button").Count;
+        
         // Assert
-        Assert.Equal("Search Levels", searchField.GetAttribute("Placeholder"));
+        Assert.Equal(expected,actual);
     }
 
     [Fact]
     public void IndexChangeSearchFieldForFilterWhenSelectingOptionProgramingLanguages()
     {
         // Arrange
-        using var ctx = new TestContext();
-        var cut = ctx.RenderComponent<Index>();
-        var filterButton = cut.FindAll("button").GetElementById("filterButton");
+        
+        var cut = _ctx.RenderComponent<Index>();
+        var expected = 13;
 
         // Act
+        var filterButton = cut.FindAll("button").GetElementById("filterButton");
         filterButton.Click();
         cut.FindAll("button").GetElementById("programming Languages").Click();
-        var searchField = cut.FindAll("input").GetElementById("searchFilter");
+        var actual = cut.FindAll("button").Count;
 
         // Assert
-        Assert.Equal("Search programming Languages", searchField.GetAttribute("Placeholder"));
+        Assert.Equal(expected, actual);
     }
 
     [Fact]
     public void IndexChangeSearchFieldForFilterWhenSelectingOptionLanguagess()
     {
         // Arrange
-        using var ctx = new TestContext();
-        var cut = ctx.RenderComponent<Index>();
-        var filterButton = cut.FindAll("button").GetElementById("filterButton");
+        var cut = _ctx.RenderComponent<Index>();
+        var expected = 11;
 
         // Act
+        var filterButton = cut.FindAll("button").GetElementById("filterButton");
         filterButton.Click();
         cut.FindAll("button").GetElementById("languages").Click();
-        var searchField = cut.FindAll("input").GetElementById("searchFilter");
+        var actual = cut.FindAll("button").Count;
 
         // Assert
-        Assert.Equal("Search Languages", searchField.GetAttribute("Placeholder"));
+        Assert.Equal(expected, actual);
     }
 
     [Fact]
     public void IndexChangeSearchFieldForFilterWhenSelectingOptionMedias()
     {
         // Arrange
-        using var ctx = new TestContext();
-        var cut = ctx.RenderComponent<Index>();
-        var filterButton = cut.FindAll("button").GetElementById("filterButton");
+        var cut = _ctx.RenderComponent<Index>();
+        var expected = 12;
 
         // Act
+        var filterButton = cut.FindAll("button").GetElementById("filterButton");
         filterButton.Click();
         cut.FindAll("button").GetElementById("medias").Click();
-        var searchField = cut.FindAll("input").GetElementById("searchFilter");
+        var actual = cut.FindAll("button").Count;
 
         // Assert
-        Assert.Equal("Search Medias", searchField.GetAttribute("Placeholder"));
+        Assert.Equal(expected, actual);
+    }
+
+    public void Dispose()
+    {
+        _ctx.Dispose();
     }
 }
-*/
