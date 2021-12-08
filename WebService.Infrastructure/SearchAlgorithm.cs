5841da970e02ee3bf25766ffa75bd2c352ba53db
anmb <anmb@itu.dk>
2021-12-08 16:20:42 +0100

    Add Status to return statement of Search. Add Filter metod to Search
    Co-authored-by: Myssenberg <Myssenberg@users.noreply.github.com>

--- WebService.Infrastructure/SearchAlgorithm.cs
+++ WebService.Infrastructure/SearchAlgorithm.cs
@@ -1,40 +1,92 @@
-﻿
+﻿using WebService.Core;
 
 namespace WebService.Infrastructure
 {
     public class SearchAlgorithm : ISearch
     {
-        public List<MaterialDTO> Search(SearchForm searchform)
-        {
-            throw new NotImplementedException();
+        private readonly IMaterialRepository _repository;
+        private SearchForm _searchForm; //SPØRG
 
-            //return new List<int>();
+        public SearchAlgorithm(IMaterialRepository materialRepository)
+        {
+            _repository = materialRepository;
+        }
+
+        public async Task<(Status, ICollection<MaterialDTO>)> Search(SearchForm searchForm)
+        {
+            _searchForm = searchForm;
+            var databaseMaterials = FindMaterials(SearchFormParse(_searchForm));
+            if (databaseMaterials.Result.Item1 == Status.NotFound)
+            {
+                return databaseMaterials.Result;
+            };
+
+            var materialDTOs = FilterMaterials(databaseMaterials.Result.Item2);
+            return (Status.Found, PrioritizeMaterials(materialDTOs));
         }
 
         public SearchForm SearchFormParse(SearchForm searchForm)
         {
-            //TextFieldParse(searchForm);
-            throw new NotImplementedException();
+            return TextFieldParse(searchForm);
         }
-        
+
         public SearchForm TextFieldParse(SearchForm searchForm)
         {
-            throw new NotImplementedException();
-            string[] text = searchForm.TextField.Split(' ');
+            return searchForm;
+            //string[] text = searchForm.TextField.Split(' ');
         }
 
-        private List<MaterialDTO> FindMaterials(SearchForm searchForm)
+        private async Task<(Status, ICollection<MaterialDTO>)> FindMaterials(SearchForm searchForm)
         {
-            throw new NotImplementedException();
+            var result = _repository.ReadAsync(searchForm).Result;
+
+            return (result.Item1, new List<MaterialDTO>(result.Item2));
         }
 
-        private List<MaterialDTO> PrioritizeMaterials(List<MaterialDTO> materials)
+        private ICollection<MaterialDTO> FilterMaterials(ICollection<MaterialDTO> materials)
         {
-            throw new NotImplementedException();
+            materials = new List<MaterialDTO>(FilterLanguage(materials));
+            materials = new List<MaterialDTO>(FilterTags(materials));
+            return materials;
         }
-        public Task<ICollection<MaterialDTO>> Search(SearchForm searchForm)
+
+
+        private ICollection<MaterialDTO> PrioritizeMaterials(ICollection<MaterialDTO> materials)
         {
-            throw new NotImplementedException();
+            //TODO this
+            return materials;
+        }
+
+        private IEnumerable<MaterialDTO> FilterLanguage(ICollection<MaterialDTO> materials)
+        {
+
+            foreach (MaterialDTO m in materials)
+            {
+                if (_searchForm.Languages.Contains(m.Language))
+                {
+                    yield return m;
+                }
+            }
+        }
+
+        private IEnumerable<MaterialDTO> FilterTags(ICollection<MaterialDTO> materials)
+        {
+            materialLoop:
+            foreach (MaterialDTO m in materials)
+            {  
+                foreach (WeightedTagDTO wt in m.Tags)
+                {
+                    foreach (TagDTO t in _searchForm.Tags)
+                    {
+                        if (wt.Name == t.Name)
+                        {
+                            yield return m;
+                            goto materialLoop;
+                        }
+                    }
+                }
+                  
+            }
         }
     }
 }
