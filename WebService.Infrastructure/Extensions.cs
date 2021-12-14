e8340388d9c1bdb08d34a1b6cd98e019a2f2841c
RasmusUK <rasmus.kritensen@live.dk>
2021-12-14 12:07:55 +0100

    nicer

---
+++ WebService.Infrastructure/Extensions.cs
@@ -0,0 +1,22 @@
+﻿namespace WebService.Infrastructure
+{
+    public static class Extensions
+    {
+        public static bool IsEqualIgnoreCasing(this string source, string toCheck)
+        {
+            return string.Equals(source, toCheck, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static bool ContainsIgnoreCasing(this string source, string toCheck)
+        {
+            return source.Contains(toCheck, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static bool ContainsIgnoreCasing(this IEnumerable<String> source, string toCheck)
+        {
+            return source.Contains(toCheck, StringComparer.OrdinalIgnoreCase);
+        }
+
+    }
+
+}
--- WebService.Infrastructure/SearchAlgorithm.cs
+++ WebService.Infrastructure/SearchAlgorithm.cs
@@ -169,26 +169,4 @@ namespace WebService.Infrastructure
         }
 
     }
-
-    public static class StringExtensions
-    {
-        public static bool IsEqualIgnoreCasing(this string source, string toCheck)
-        {
-            return string.Equals(source, toCheck, StringComparison.OrdinalIgnoreCase);
-        }
-
-        public static bool ContainsIgnoreCasing(this string source, string toCheck)
-        {
-            return source.Contains(toCheck, StringComparison.OrdinalIgnoreCase);
-        }
-
-        public static bool ContainsIgnoreCasing(this IEnumerable<String> source, string toCheck)
-        {
-            return source.Contains(toCheck, StringComparer.OrdinalIgnoreCase);
-        }
-
-    }
-
-
-
 }
