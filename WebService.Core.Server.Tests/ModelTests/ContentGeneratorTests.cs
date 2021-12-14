604ba6fdb704ce7c8d278cc847f0cadc1783209f
Thor Lyster Lind <thll@itu.dk>
2021-12-14 14:27:50 +0100

    refactoring of seeding

--- WebService.Core.Server.Tests/ModelTests/ContentGeneratorTests.cs
+++ WebService.Core.Server.Tests/ModelTests/ContentGeneratorTests.cs
@@ -1,120 +1,116 @@
 ﻿using System;
 using System.Linq;
+using WebService.Core.Server.Model;
 
 namespace WebService.Core.Server.Tests.ModelTests;
+
 public class ContentGeneratorTests
 {
-	private ContentGenerator _gen = new ContentGenerator();
-	public ContentGeneratorTests()
-	{
-	}
-
-
-	[Fact]
-	public void GenerateTitle_given_no_tags_should_return_success_false()
+    [Fact]
+    public void GenerateTitle_given_no_tags_should_return_success_false()
     {
-		var tags = new List<CreateWeightedTagDTO>();
+        var tags = new List<CreateWeightedTagDTO>();
 
-		var (ok, _) = _gen.GenerateTitle(tags);
+        var (ok, _) = ContentGenerator.GenerateTitle(tags);
 
-		Assert.False(ok);
+        Assert.False(ok);
     }
 
-	[Fact]
-	public void GenerateTitle_given_tag_docker_should_return_title_containing_docker()
+    [Fact]
+    public void GenerateTitle_given_tag_docker_should_return_title_containing_docker()
     {
-		var tagName = "docker";
-		var tags = CreateTags(tagName);
-		Assert.Single(tags);
+        const string tagName = "docker";
+        var tags = CreateTags(tagName);
+        Assert.Single(tags);
 
-		var (ok, title) = _gen.GenerateTitle(tags);
+        var (ok, title) = ContentGenerator.GenerateTitle(tags);
 
-		Assert.True(ok);
-		Assert.Contains(tagName, title);
-		Assert.True(tagName.Length < title.Length);
+        Assert.True(ok);
+        Assert.Contains(tagName, title);
+        Assert.True(tagName.Length < title.Length);
     }
 
-	[Fact]
-	public void GenerateTitle_given_tags_docker_kubernetes_return_title_containing_both()
+    [Fact]
+    public void GenerateTitle_given_tags_docker_kubernetes_return_title_containing_both()
     {
-		var tagNames = new string[]
-		{
-			"docker",
-			"kubernetes"
-		};
-		var tags = CreateTags(tagNames);
-
-		var (ok, title) = _gen.GenerateTitle(tags);
-
-		Assert.True(ok);
-		Assert.True(tagNames.Sum(n => n.Length) < title.Length);
-		foreach (var tag in tagNames)
+        var tagNames = new[]
         {
-			Assert.Contains(tag, title);
-        }
+            "docker",
+            "kubernetes"
+        };
+        var tags = CreateTags(tagNames);
 
-		Assert.Contains("docker and kubernetes", title);
+        var (ok, title) = ContentGenerator.GenerateTitle(tags);
+
+        Assert.True(ok);
+        Assert.True(tagNames.Sum(n => n.Length) < title.Length);
+        foreach (var tag in tagNames) Assert.Contains(tag, title);
+
+        Assert.Contains("docker and kubernetes", title);
     }
 
-	[Fact]
-	public void GenerateTitle_given_tags_docker_kubernetes_azure_return_title_containing_all_with_comma_and_and_separation()
+    [Fact]
+    public void
+        GenerateTitle_given_tags_docker_kubernetes_azure_return_title_containing_all_with_comma_and_and_separation()
     {
-		var tagNames = new string[]
-		{
-			"docker",
-			"kubernetes",
-			"azure"
-		};
-		var tags = CreateTags(tagNames);
+        var tagNames = new[]
+        {
+            "docker",
+            "kubernetes",
+            "azure"
+        };
+        var tags = CreateTags(tagNames);
 
-		var (ok, title) = _gen.GenerateTitle(tags);
+        var (ok, title) = ContentGenerator.GenerateTitle(tags);
 
-		Assert.True(ok);
-		Assert.True(tagNames.Sum(n => n.Length) < title.Length);
-		Assert.Contains("docker, kubernetes and azure", title);
+        Assert.True(ok);
+        Assert.True(tagNames.Sum(n => n.Length) < title.Length);
+        Assert.Contains("docker, kubernetes and azure", title);
     }
 
 
-	[Theory]
-	[InlineData(ContentGenerator.Language.ENGLISH, "english")]
-	[InlineData(ContentGenerator.Language.RUSSIAN, "russian")]
-	[InlineData(ContentGenerator.Language.DANISH, "danish")]
-	[InlineData(ContentGenerator.Language.ITALIAN, "italian")]
-	public void LanguageToString_converts_correctly_from_language_to_string(ContentGenerator.Language lang, string expected)
+    [Theory]
+    [InlineData(ContentGenerator.Language.English, "english")]
+    [InlineData(ContentGenerator.Language.Russian, "russian")]
+    [InlineData(ContentGenerator.Language.Danish, "danish")]
+    [InlineData(ContentGenerator.Language.Italian, "italian")]
+    public void LanguageToString_converts_correctly_from_language_to_string(ContentGenerator.Language lang,
+        string expected)
     {
-		var (ok, actual) = _gen.LanguageToString(lang);
+        var (ok, actual) = ContentGenerator.LanguageToString(lang);
 
-		Assert.True(ok);
-		Assert.Equal(expected, actual);
+        Assert.True(ok);
+        Assert.Equal(expected, actual);
     }
 
-	[Theory]
-	[InlineData("danish", ContentGenerator.Language.DANISH)]
-	[InlineData("english", ContentGenerator.Language.ENGLISH)]
-	[InlineData("italian", ContentGenerator.Language.ITALIAN)]
-	[InlineData("russian", ContentGenerator.Language.RUSSIAN)]
-	public void StringToLanguage_converts_magical_strings_to_languages(string input, ContentGenerator.Language expected)
+    [Theory]
+    [InlineData("danish", ContentGenerator.Language.Danish)]
+    [InlineData("english", ContentGenerator.Language.English)]
+    [InlineData("italian", ContentGenerator.Language.Italian)]
+    [InlineData("russian", ContentGenerator.Language.Russian)]
+    public void StringToLanguage_converts_magical_strings_to_languages(string input, ContentGenerator.Language expected)
     {
-		var (ok, actual) = _gen.StringToLanguage(input);
+        var (ok, actual) = ContentGenerator.StringToLanguage(input);
 
-		Assert.True(ok);
-		Assert.Equal(expected, actual);
+        Assert.True(ok);
+        Assert.Equal(expected, actual);
     }
 
 
-	[Theory]
-	[InlineData("")]
-	[InlineData("     ")]
-	[InlineData("Volapyk")]
-	[InlineData(null)]
-	public void StringToLanguage_does_not_convert_wrong_magical_strings_to_languages(string input)
+    [Theory]
+    [InlineData("")]
+    [InlineData("     ")]
+    [InlineData("Volapük")]
+    [InlineData(null)]
+    public void StringToLanguage_does_not_convert_wrong_magical_strings_to_languages(string input)
     {
-		var (ok, _) = _gen.StringToLanguage(input);
+        var (ok, _) = ContentGenerator.StringToLanguage(input);
 
-		Assert.False(ok);
+        Assert.False(ok);
     }
 
-	private static IList<CreateWeightedTagDTO> CreateTags(params string[] tagNames)
-		=> Array.ConvertAll<string, CreateWeightedTagDTO>(tagNames, tag => new CreateWeightedTagDTO(tag, 0));
-    
+    private static IList<CreateWeightedTagDTO> CreateTags(params string[] tagNames)
+    {
+        return Array.ConvertAll(tagNames, tag => new CreateWeightedTagDTO(tag, 0));
+    }
 }
