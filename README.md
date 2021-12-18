9a09e4169b3abb01bd7917bc997a009a2c9aded5
Andreas Wachs <andreas@wachs.dk>
2021-12-18 14:08:40 +0100

    Add start script and change README (#97)
    Also fixed a small bug with authors name generation

--- README.md
+++ README.md
@@ -1,14 +1,32 @@
 # BDSA2021-FinalProject
 This is the final project for the BDSA2021 course at ITU
 
+## Prerequisites
 
-## Test user login
+In order to run this project, you need to have the following pieces of software installed, working and running:
 
+- Docker
+- dotNET Core 6.0
+- Powershell
+
+## Run the project
+
+To run the project, run the `start.ps1` file:
+
+```pwsh
+.\start.ps1
+```
+
+It takes just a few minutes for the system to be fully running. After asking docker to start the DB container, the script waits 30 seconds. After that the server starts up and populates the DB, which takes a bit of time.
+
+## Development
+
+### Test user login
 
 Email: test@itubdsabridgethegap.onmicrosoft.com
 Password: 42_ER_1_TAL
 
-## Running the database
+### Running the database
 
 This will generate a new connection string and store it as a user secret. Then it will run a Postgres DBMS docker container.
 
@@ -18,14 +36,13 @@ TODO: How to properly initialize the DB with perhaps seed data..
 .\start_db.ps1
 ```
 
-
-## Making migrations
+### Making migrations
 
 ```pwsh
 .\migrate.ps1 NAME
 ``` 
 
-## Updating the database
+### Updating the database
 
 ```pwsh
 .\update_db.ps1
---
+++ start.ps1
@@ -0,0 +1,45 @@
+
+Write-Host "###########################################################################"
+Write-Host "      __/\\\\\\\\\\\\\____/\\\\\\\\\\\\\\\_____/\\\\\\\\\\\\_        "
+Write-Host "       _\/\\\/////////\\\_\///////\\\/////____/\\\//////////__       "
+Write-Host "        _\/\\\_______\/\\\_______\/\\\________/\\\_____________      "
+Write-Host "         _\/\\\\\\\\\\\\\\________\/\\\_______\/\\\____/\\\\\\\_     "
+Write-Host "          _\/\\\/////////\\\_______\/\\\_______\/\\\___\/////\\\_    "
+Write-Host "           _\/\\\_______\/\\\_______\/\\\_______\/\\\_______\/\\\_   "
+Write-Host "            _\/\\\_______\/\\\_______\/\\\_______\/\\\_______\/\\\_  "
+Write-Host "             _\/\\\\\\\\\\\\\/________\/\\\_______\//\\\\\\\\\\\\/__ "
+Write-Host "              _\/////////////__________\///_________\////////////____"
+Write-Host "###########################################################################"
+Write-Host "Starting the Better Than Google project"
+Write-Host "Please ensure that port 1433 is not in use on your local machine"
+Write-Host "If you are using a firewall, please ensure that port 1433 is not blocked"
+Write-Host "###########################################################################"
+
+$username = "sa"
+$password = New-Guid
+$database = "btg"
+$dbhost = "localhost"
+$connectionString = "Server=$dbhost;Database=$database;User Id=$username;Password=$password;Trusted_Connection=False;Encrypt=False"
+
+Write-Host "Initializing user secrets and setting database connection string"
+dotnet user-secrets init --project .\WebService.Core\Server
+dotnet user-secrets set "ConnectionStrings:$database" "$connectionString" --project WebService.Core/Server
+
+Write-Host "Pulling the mssql image from Docker Hub"
+docker pull mcr.microsoft.com/mssql/server:2019-latest
+
+Write-Host "Starting the DB container"
+docker kill bdsa2021_btg_db
+docker rm bdsa2021_btg_db
+docker run --name bdsa2021_btg_db -d -ti -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$password" -p 1433:1433 mcr.microsoft.com/mssql/server:2019-latest
+
+Write-Host "Sleeping for 30 seconds to allow the DB to start"
+Start-Sleep -Seconds 30
+
+Write-Host "Updating the database"
+dotnet ef database update -s WebService.Core\Server --project .\WebService.Entities\
+
+Write-Host "Starting the server"
+dotnet run --project .\WebService.Core\Server
+
+
